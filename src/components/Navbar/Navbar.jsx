be10ef3e2987e5ae82e76311f3e9e05e4eb1d256
jlp21 <portelli21@gmail.com>
2021-12-04 11:17:18 -0500

    starting

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+// import { Link } from 'react-router-dom';
+import './Navbar.css';
+
+class Navbar extends Component {
+    render() {
+        return(
+            <div>Navbar</div>
+        )
+    }
+}
+
+
+export default Navbar;
